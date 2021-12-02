2b7be8e84a80fced8c809d97c95c4c0c7f17e77c
Nguyễn Tiến Anh <anhtien2k00@gmail.com>
2021-12-02 17:58:55 +0700

    demo-test

---
+++ pages/home/index.tsx
@@ -0,0 +1,15 @@
+import { type } from "os"
+
+export default function test(){
+    return (
+        <>
+            <div>
+                <ul>
+                    <li>Babae</li>
+                    <li>Babae1</li>
+                    <li>Babae2</li>
+                </ul>
+            </div>
+        </>
+    )
+}
